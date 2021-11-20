8ab97ac047c11f7ceb54ad07a55e5e89f2a89264
mayabartels <mayabartels01@gmail.com>
2021-11-19 16:40:25 -0800

    Delete ScoreUpdate (in other function for finding score for turn)

--- scoreUpdate.m
+++
@@ -1,6 +0,0 @@
-function [updateScore] = scoreUpdate(currentScore, roundScore)
-    
-    % Take previous score and add the new score (only if score not reset)
-    updateScore = currentScore + roundScore;
-
-end

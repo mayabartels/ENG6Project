265be992f10dfdfdb1ed6c314cce120bc312bd09
rowan-glenn <94141262+rowan-glenn@users.noreply.github.com>
2021-11-24 16:33:16 -0800

    updated scoring system

--- getPlayer1Score.m
+++ getPlayer1Score.m
@@ -1,10 +1,11 @@
 function  [data1]= getPlayer1Score(playerRoundNum, Player1Scores)
 
 roundArray = (1:playerRoundNum)'
+
 scoreArray = (Player1Scores)'
 
-together = [roundArray, scoreArray];
-data1 = together;
+together = [(roundArray), (scoreArray)];
+data1 = num2cell(together);
 
 
 
--- getPlayer2Score.m
+++ getPlayer2Score.m
@@ -1,10 +1,12 @@
 function  [data2]= getPlayer2Score(playerRoundNum, Player2Scores)
 
 roundArray = (1:playerRoundNum)'
+
 scoreArray = (Player2Scores)'
 
-together = [roundArray, scoreArray];
-data2 = together;
+together = [(roundArray), (scoreArray)];
+data2 = num2cell(together);
+
 
 
 
